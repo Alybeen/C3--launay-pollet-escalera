9a47591d42d45b6b165437e41a59673690d73e2b
Alice Escalera <alice.escalera@gmail.com>
2024-03-11 11:30:28 +0100

    write tdd test to check each team has an equal number of players

--- test/teamGenerator.test.js
+++ test/teamGenerator.test.js
@@ -2,9 +2,12 @@ import { expect } from 'chai';
 import TeamGenerator from "../src/teamGenerator.js";
 
 describe('Création d\'une équipe', () => {
+    let players;
+
     it('Création Simple : Vérification des paramètres de base', () => {
+        players = ['Player1', 'Player2', 'Player3', 'Player4', 'Player5', 'Player6'];
         // create players
-        const players = ['Player 1', 'Player 2', 'Player 3', 'Player 4', 'Player 5'];
+
 
         // generate team
         const teamGenerator = new TeamGenerator(players);
@@ -18,5 +21,24 @@ describe('Création d\'une équipe', () => {
         const firstTeam = teams[0];
         expect(firstTeam).to.have.property('name').that.is.a('string');
         expect(firstTeam).to.have.property('players').that.is.an('array').that.is.not.empty;
+    });
+
+    //test tdd -> test if each team has an equal numbers of players
+    it('check if each team has equal numbers of players ', () => {
+
+        players = ['Player1', 'Player2', 'Player3', 'Player4', 'Player5', 'Player6', 'Player7'];
+
+        const generator = new TeamGenerator(players, 3);
+        generator.generateTeams();
+
+        const teams = generator.getTeams();
+
+        expect(teams).to.be.an('array');
+        expect(teams).to.have.lengthOf(Math.ceil(players.length / 3));
+
+        teams.forEach(team => {
+            expect(team.players).to.be.an('array');
+            expect(team.players).to.have.lengthOf(3);
+        });
     });
 })
