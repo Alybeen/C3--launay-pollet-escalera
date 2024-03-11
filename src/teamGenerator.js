051721f44436fd9f7f883c95d6027870bf0a39b0
Alice Escalera <alice.escalera@gmail.com>
2024-03-11 11:52:06 +0100

    add function to check number of players comparing to team number

--- src/teamGenerator.js
+++ src/teamGenerator.js
@@ -4,20 +4,26 @@ class TeamGenerator {
     this.playersPerTeam = playersPerTeam;
     this.teams = [];
   }
+  isPlayersCountDivisible(players, teamsCount) {
+    return playersCount % teamsCount === 0;
+  }
 
   generateTeams() {
     let shuffledPlayers = [...this.players].sort(() => 0.5 - Math.random()); // Mélange aléatoire des joueurs
     let teamIndex = 0;
-
-    while (shuffledPlayers.length > 0) {
-      let teamPlayers = shuffledPlayers.splice(0, this.playersPerTeam);
-      let teamName = `Équipe ${teamIndex + 1}`;
-      let team = {
-        name: teamName,
-        players: teamPlayers,
-      };
-      this.teams.push(team);
-      teamIndex++;
+    if(isPlayersCountDivisible===0) {
+      while (shuffledPlayers.length > 0) {
+        let teamPlayers = shuffledPlayers.splice(0, this.playersPerTeam);
+        let teamName = `Équipe ${teamIndex + 1}`;
+        let team = {
+          name: teamName,
+          players: teamPlayers,
+        };
+        this.teams.push(team);
+        teamIndex++;
+      }
+    } else {
+      console.log("impossible de créer des équipes avec le même nombre de joueurs")
     }
   }
 
